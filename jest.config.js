06d58c77d8c01b3824f061896191bd7efaf38998
mehmetalicayhan <mehmetalicyhn@gmail.com>
2021-06-15 03:32:37 +0300

    test configs for jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  transform: {
+    '^.+\\.svg$': '<rootDir>/svgTransform.js',
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.vue$': 'vue-jest',
+  },
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/tests/unit/coverage',
+  collectCoverageFrom: [
+    'src/**/*.(js|vue)',
+  ],
+  moduleFileExtensions: [
+    'js',
+    'vue',
+  ],
 };
---
+++ svgTransform.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const vueJest = require('vue-jest/lib/template-compiler');
+
+module.exports = {
+  process(content) {
+    const { render } = vueJest({
+      content,
+      attrs: {
+        functional: false,
+      },
+    });
+
+    return `module.exports = { render: ${render} }`;
+  },
+};
