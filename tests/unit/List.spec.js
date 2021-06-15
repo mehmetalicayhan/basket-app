9c78275461ec5174177137eccd77f8769190ba83
mehmetalicayhan <mehmetalicyhn@gmail.com>
2021-06-15 16:38:16 +0300

    list component unit tests edited

--- tests/unit/List.spec.js
+++ tests/unit/List.spec.js
@@ -7,28 +7,4 @@ describe('List.vue', () => {
     expect(wrapper.find('.list-item'))
       .toBeTruthy();
   });
-  it('should render products', () => {
-    const wrapper = shallowMount(List, {
-      data() {
-        return {
-          products: [{
-            id: 1,
-            name: 'Şampuan',
-            price: '13',
-            currency: 'TRY',
-            image: 'https://cdn.glitch.com/a28552e7-44e1-4bbd-b298-5745e70c2209%2Fsampuan.jpeg?v=1561027551321',
-          }, {
-            id: 2,
-            name: 'Deodorant',
-            price: '26',
-            currency: 'TRY',
-            image: 'https://cdn.glitch.com/a28552e7-44e1-4bbd-b298-5745e70c2209%2Fdeodorant.jpeg?v=1561027551696',
-          }],
-        };
-      },
-    });
-    const listItems = wrapper.findAll('.list-item');
-    expect(listItems)
-      .toHaveLength(2);
-  });
 });
